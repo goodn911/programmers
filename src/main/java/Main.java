8629d94dd4921bcdb49f01d0628973ee6264ceed
goodn911 <90505402+goodn911@users.noreply.github.com>
2021-11-08 19:28:36 +0900

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int solution(int[] numbers) {
+        
+        int answer = -1;
+        for(int i = 0; i<10;i++){
+            int number = numbers[i];
+        }
+        return answer;
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        // write your code here
+        Solution solution = new Solution();
+        System.out.println(solution.solution(3,5));
+
+    }
+}

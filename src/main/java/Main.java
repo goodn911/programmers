9eff27b63205f67385aff8a48ba233b1d65d636c
goodn911 <90505402+goodn911@users.noreply.github.com>
2021-11-12 18:30:08 +0900

    행렬의 덧셈

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -191,4 +191,31 @@ public class Main {
 
  */
 
+//행렬의 덧셈
+class Solution {
+    public int[][] solution(int[][] arr1, int[][] arr2) {
+        int[][] answer = new int[arr1.length][arr2[0].length];
+        for(int i =0;i<arr1.length;i++){
+            for(int j = 0;j<arr2[i].length;j++){
+                answer[i][j] = arr1[i][j] + arr2[i][j];
 
+            }
+        }
+        return answer;
+    }
+}
+public class Main {
+    public static void main(String[] args) {
+        // write your code here
+        int[][] aa = {
+                {1,2},
+                {2,3}
+        };
+        int[][] bb = {
+                {3,4},
+                {5,6}
+        };
+        Solution solution = new Solution();
+        System.out.println(solution.solution(aa,bb));
+    }
+}
